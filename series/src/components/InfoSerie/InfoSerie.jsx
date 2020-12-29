45795175821515507e39f8377a3999719a86b961
thiagonmiziara <thiagonmiziara@gmail.com>
2020-12-29 14:25:02 -0300

    adicionando a funçao de editar as series

--- series/src/components/InfoSerie/InfoSerie.jsx
+++ series/src/components/InfoSerie/InfoSerie.jsx
@@ -1,26 +1,45 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
+import { Badge, Button } from "reactstrap";
 
 const InfoSerie = ({ match }) => {
-  const [name, setName] = useState("");
+  const [form, setForm] = useState({});
   const [success, setSuccess] = useState(false);
+  const [mode, setMode] = useState("INFO");
   const [data, setData] = useState({});
 
   useEffect(() => {
     axios
       .get("/api/series/" + match.params.id)
-      .then((res) => setData(res.data));
+      .then((res) => {
+        setData(res.data);
+        setForm(res.data);
+      });
   }, [match.params.id]);
 
-  const hadleChange = (event) => {
-    setName(event.target.value);
+  //custom header
+
+  const masterHeader = {
+    heigth: "50vh",
+    minHiegth: "500px",
+    backgroundImage: `url('${data.background}')`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+
+  const hadleChange = (field)=> (event) => {
+    setForm({
+      ...form,
+      [field]: event.target.value
+    });
   };
 
   const save = () => {
     axios
       .post("/api/series", {
-        name,
+        form,
       })
       .then((res) => {
         setSuccess(true);
@@ -33,10 +52,10 @@ const InfoSerie = ({ match }) => {
 
   return (
     <div>
-      <header>
-        <div className="h-100" style={{ background: "rgba(0,0,0,0.7)" }}>
+      <header style={masterHeader}>
+        <div className="h-100" style={{ background: "rgba(0,0,0,0.6)" }}>
           <div className="h-100 container">
-            <div className="row">
+            <div className="row h-100 align-items-center">
               <div className="col-3">
                 <img
                   className="img-fluid img-thumbnail"
@@ -44,31 +63,55 @@ const InfoSerie = ({ match }) => {
                   alt={data.name}
                 />
               </div>
-              <div className="col-8">Titulo</div>
+              <div className="col-8">
+                <h1 className="font-weight-light text-white">{data.name}</h1>
+                <div className="lead text-white">
+                  <Badge color="success">Assistido</Badge>
+                  <Badge color="warning">Para assistir</Badge>
+                  Gênero:{data.genre}
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </header>
-      <div className="container">
-        <h1>Nova Série</h1>
-        <pre>{JSON.stringify(data)}</pre>
-        <form>
-          <div className="form-group">
-            <label htmlFor="name">Nome</label>
-            <input
-              type="text"
-              value={name}
-              onChange={hadleChange}
-              className="form-control"
-              id="name"
-              placeholder="Nome do Genêro"
-            />
-          </div>
-          <button onClick={save} type="button" className="btn btn-primary">
-            Salvar
-          </button>
-        </form>
+      <div>
+        <Button color='primary'onClick={()=> setMode('EDIT')}>Editar</Button>
       </div>
+      {mode === "EDIT" && (
+        <div className="container">
+          <h1>Nova Série</h1>
+        <Button color='warning' onClick={()=> setMode('INFO')}>Cancelar Edição</Button>
+
+          <form>
+            <div className="form-group">
+              <label htmlFor="name">Nome</label>
+              <input
+                type="text"
+                value={form.name}
+                onChange={hadleChange('name')}
+                className="form-control"
+                id="name"
+                placeholder="Nome do Genêro"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Comentários</label>
+              <input
+                type="text"
+                value={form.comments}
+                onChange={hadleChange('comments')}
+                className="form-control"
+                id="name"
+                placeholder="comentários"
+              />
+            </div>
+            <button onClick={save} type="button" className="btn btn-primary">
+              Salvar
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
